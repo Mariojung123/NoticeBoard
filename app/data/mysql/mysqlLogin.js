921759a3ca4575e850f9388b1aafa8935b22b01e
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-11-21 22:50:56 +0900

    Revert "로그인 후 세션 유지"
    This reverts commit ed8ab5889dda4174885fc6858263b05b8a4f3d1d.

--- app/data/mysql/mysqlLogin.js
+++ app/data/mysql/mysqlLogin.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 router.use(session({
   secret: process.env.SessionSecret, // 임의의 비밀 키, 실제 프로덕션에서는 더 안전한 방식을 사용해야 합니다.
-  resave: true,
+  resave: false,
   saveUninitialized: true,
   cookie: { secure: false } // HTTPS를 사용하는 경우 true로 설정해야 합니다.
 }));
@@ -21,6 +21,10 @@ const DBCP = {
         const [rows] = connection.query('SELECT * FROM userTable WHERE id = ? AND password = ?', [login_id, login_pw]);
 
       if (rows.length > 0) {
+<<<<<<< HEAD
+=======
+        req.session.user = { login_id, login_pw };
+>>>>>>> parent of ed8ab58 (로그인 후 세션 유지)
         res.send('로그인 성공');
       } else {
         res.send('로그인 실패');
@@ -35,4 +39,8 @@ const DBCP = {
   }
 };
 
+<<<<<<< HEAD
 module.exports = DBCP;
+=======
+module.exports = DBCP;
+>>>>>>> parent of ed8ab58 (로그인 후 세션 유지)
