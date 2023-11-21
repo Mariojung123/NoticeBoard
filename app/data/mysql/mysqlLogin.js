9f2371d03849d753f4f1a14a4be765ae2e7ffa36
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-11-21 23:06:34 +0900

    로그인 시점으로 복구
    코드 개꼬여서 갈아버릴라다가 못 찾아서
    진짜 코드 다 날리는줄 알고 식은땀 졸라 났다

--- app/data/mysql/mysqlLogin.js
+++ app/data/mysql/mysqlLogin.js
@@ -1,38 +1,31 @@
 const express = require('express');
-const session = require('express-session');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const db = require('./mysqlConnection');
-require('dotenv').config();
 
 
 const DBCP = {
-  connection: function (req, res, next) { // next 파라미터를 추가해야 합니다.
-    // pool.getConnection()의 반환 값을 변수에 저장
-    const { login_id, login_pw } = req.body;
-    db.pool.getConnection().then((connection) => {
-      try {
-        connection.query('SELECT * FROM userTable WHERE id = ? AND password = ?', [login_id, login_pw], (error, rows) => {
-          if (error) {
-            console.error(error);
-            res.status(500).send('서버 오류');
-            return;
-          }
+  connection: async function (req, res, next) {
+    try {
+      // pool.getConnection()의 반환 값을 변수에 저장
+      const { login_id, login_pw } = req.body;
+      console.log(login_id, login_pw);
 
-          if (rows.length > 0) {
-            res.send('로그인 성공');
-          } else {
-            res.send('로그인 실패');
-          }
+      const connection = await db.pool.getConnection();
+      const [rows, fields] = await connection.query('SELECT * FROM userTable WHERE username = ? AND password = ?', [login_id, login_pw]);
 
-          connection.release();
-          next(); // next()를 이 위치에 올바르게 호출해야 합니다.
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('서버 오류');
+      if (rows.length > 0) {
+        res.send('로그인 성공');
+      } else {
+        res.send('로그인 실패');
       }
-    });
+
+      connection.release();
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('서버 오류');
+    }
   }
 };
 
--- app/data/mysql/router.js
+++ app/data/mysql/router.js
@@ -1,6 +1,6 @@
-const session = require('./session');
 const express = require('express');
 const router = express.Router();
+
 const DBCP = require('./mysqlLogin');
 
 router.use('/dbcp', DBCP.connection);
