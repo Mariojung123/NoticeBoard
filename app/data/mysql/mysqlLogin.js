8d928818788d7e1fb7cdd4dfc6851c31c564dbed
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-11-21 22:48:38 +0900

    Revert "DBCP 로그인 구현 완료"
    This reverts commit 0a189818472f4e1d02db3d5cb6d9d9815a87233f.

--- app/data/mysql/mysqlLogin.js
+++ app/data/mysql/mysqlLogin.js
@@ -13,39 +13,26 @@ router.use(session({
 }));
 
 const DBCP = {
-  connection: async function (req, res, next) {
-    try {
-      // pool.getConnection()의 반환 값을 변수에 저장
-      const { login_id, login_pw } = req.body;
-      console.log(login_id, login_pw);
-
-      const connection = await db.pool.getConnection();
-      const [rows, fields] = await connection.query('SELECT * FROM userTable WHERE username = ? AND password = ?', [login_id, login_pw]);
+  connection: function (req, res) {
+    // pool.getConnection()의 반환 값을 변수에 저장
+    const { login_id, login_pw } = req.body;
+    db.pool.getConnection().then((connection) => {
+      try {
+        const [rows] = connection.query('SELECT * FROM userTable WHERE id = ? AND password = ?', [login_id, login_pw]);
 
       if (rows.length > 0) {
-        // req.session.user = { login_id, login_pw };
-        next(); // next()를 res.send 이후에 호출
+        res.send('로그인 성공');
       } else {
         res.send('로그인 실패');
       }
 
       connection.release();
+      next();
     } catch (error) {
       console.error(error);
       res.status(500).send('서버 오류');
     }
-  },
-  logout: function (req, res) {
-    req.session.destroy((err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('서버 오류');
-      } else {
-        res.send('로그아웃 성공');
-      }
-    });
   }
 };
-
 
 module.exports = DBCP;
