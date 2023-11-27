2e7268d4447db46ea068f34baeb3acdcc3526542
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-11-27 23:43:20 +0900

    user검색 구현
    거의 완

--- app/api/profile/searchUser.js
+++ app/api/profile/searchUser.js
@@ -3,10 +3,32 @@ const router = express.Router();
 
 const db = require('../mysql/mysqlConnection');
 
-const pageMove = {
-    searchUser: (req, res) => {
-        res.render('searchUser');
+const GetProfile = {
+    SearchProfile: async function (req, res) {
+        let connection;
+        try {
+            const searchName = req.query.searchName;
+            console.log(searchName);
+
+            if (!searchName) {
+                // 검색어가 제공되지 않은 경우 처리
+                return res.status(400).send('검색어를 입력하세요.');
+            };
+            connection = await db.pool.getConnection();
+
+            const [rows, fields] = await db.pool.query('SELECT * FROM userTable WHERE username = ?', [searchName]);
+        
+            res.render('searchResult', { searchName, searchResults: rows });
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).send('내부 서버 오류');
+        } finally {
+            if(connection) {
+                connection.release();
+            }
+        }
     }
 };
 
-module.exports = pageMove;
+module.exports = GetProfile;
--- app/src/routers/profile/router.js
+++ app/src/routers/profile/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../../../api/profile/searchUser');
+const userCtrl = require('../../../api/profile/User.Ctrl');
 
 
 const Profile = require('../../../api/profile/myprofile');
+const Result = require('../../../api/profile/searchUser');
 
 router.use('/myinfo', Profile.myProfile);
+router.use('/searchResult', Result.SearchProfile);
 router.use('/searchUser', userCtrl.searchUser);
 
 
