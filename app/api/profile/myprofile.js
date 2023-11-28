4d2016ef39aeb4b992776ea03f50cb836ed91414
Mariojung123 <115441849+Mariojung123@users.noreply.github.com>
2023-11-28 21:16:30 +0900

    Update myprofile.js

--- app/api/profile/myprofile.js
+++ app/api/profile/myprofile.js
@@ -8,31 +8,20 @@ const GetProfile  = {
         let connection;
         try {
             const user = req.cookies.user.login_id;
-
             if (!user) {
                 return res.status(401).send('Unauthorized');
             }
-
             connection = await db.pool.getConnection();
-
             console.log(user)
-            
             const [infos, fields] = await connection.query('SELECT * FROM userTable WHERE username = ?', [user]);
-
             connection.release();
-
             infos.forEach(row => { //출력문
                 console.log(row);
               });
-
-
-
             if (infos.length === 0) {
                 return res.status(404).send('사용자를 찾을 수 없음');
             }
-
             const userInfo = infos[0];
-
             res.render('profile', { userInfo });
         } catch (error) {
             console.error(error);
